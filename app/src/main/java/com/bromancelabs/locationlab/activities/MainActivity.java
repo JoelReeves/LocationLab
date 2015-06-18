563d9f19ffa62726b74873b60cc24f23bb9c80c1
JoelReeves <joel@bromancelabs.com>
2015-06-18 16:44:33 -0400

    Started implementation to obtain device's latitude

--- app/src/main/java/com/bromancelabs/locationlab/activities/MainActivity.java
+++ app/src/main/java/com/bromancelabs/locationlab/activities/MainActivity.java
@@ -1,18 +1,90 @@
 package com.bromancelabs.locationlab.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.bromancelabs.locationlab.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final long LOCATION_UPDATE_INTERVAL = 1000;
+    private static final long LOCATION_FASTEST_UPDATE_INTERVAL = 5000;
+    private TextView txtLocation;
+    private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Created instance of Google API client using Location Services
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        txtLocation = (TextView) findViewById(R.id.txtLocation);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // connect the client
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        // disconnect the client
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        // sets up a location request w/ high accuracy and update interval in milliseconds
+        // also sets the fastest rate in milliseconds which the app can handle location updates
+        locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(LOCATION_UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(LOCATION_FASTEST_UPDATE_INTERVAL);
+
+        // starts the request for location updates
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        final String locationLatitude = String.valueOf(location.getLatitude());
+        txtLocation.setText(locationLatitude);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "GoogleApiClient connection suspended");
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "GoogleApiClient connection failed");
     }
 
     @Override
